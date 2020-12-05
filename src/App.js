f7bea991bd16a94b3d5aa76960fb21106bff2f34
Mitch4sho <mitchelloliver1991@gmail.com>
2020-12-05 14:10:09 -0800

    Intial commit and added in the navigation bar and set up React Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import AboutMe from "./Components/AboutMe/About";
+import Contact from "./Components/ContactInformation/Contact";
+import Project from "./Components/ProjectExperience/Project";
+import Work from "./Components/WorkExperience/Work"; 
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+
+        {/* Paths for Links */}
+        <Route exact path = "/" components = {AboutMe} />
+        <Route path = "/contact" components = {Contact}/>
+        <Route path = "/projects" components = {Project}/>
+        <Route path = " /work" components = {Work}/>
+
+        <header>
+          <nav className = "navigation">
+            <div><Link className = "logo">Mitchell Oliver</Link></div>
+            <div className = "navigationSub">
+              <ol>
+                <li><Link to ="/" className="item">About</Link></li>
+                <li><Link to ="/contact" className="item">Experience</Link></li>
+                <li><Link to ="/projects" className="item">Work</Link></li>
+                <li><Link to ="/work" className="item">Contact</Link></li>
+              </ol>
+              <div>
+                <a href="#" className="resumeButton">Resume</a>
+              </div>
+            </div>
+          </nav>
+        </header>
+        </div>
+      </Router>
   );
 }
 
